034ee44db0f2b5591f6ac73e4ce4ca08efd9e39c
{[(0_0)]} <60839849+anuanu0-0@users.noreply.github.com>
2021-08-21 16:53:56 +0530

    Update MinStack.h

--- MinQueues/MinStack.h
+++ MinQueues/MinStack.h
@@ -1,82 +1,85 @@
+/*
+    * Design a stack class to support push, pop, empty, size, top & getMin operations.
+    * All operations must be in O(1)
+    * Use vector as underlying container for the stack. 
+*/
+
 #ifndef MINSTACK_H
 #define MINSTACK_H
-#include <vector>
-#include <stack>
+#include<vector>
 
 using namespace std;
 
-template <class T>
-class MinStack
-{
-public:
-    //get element at top of the stack
-    T top();
-    //get smallest element currently in stack
-    T getMin();
-    //push x on top of the stack
-    void push(T x);
-    //remove topmost element from the stack
-    void pop();
-    //check if the stack is empty
-    bool empty()
-    {
-        return myStack.empty();
-    }
-    //returns number of elements currently in stack
-    int size()
-    {
-        return myStack.empty();
-    }
-
+template<class T>
+class MinStack{
+    /*
+        Creating 2 vectors, one for main stack and one auxilary stack for keeping
+        track of the min elements. The aux stack stores only upcoming equal or lower values
+        and thus maintains min element at the top of the stack.
+    */
 private:
-    //STL stack that backs up our minStack
-    stack<pair<T, T>> myStack;
+    vector<int>mstack, aux;
+public: 
+    // Get the top of the stack
+    T top();
+    // Get the minimum element currently in the stack
+    T getMin();
+    // Push x on top of stack
+    void push(T x);
+    // Remove topmost element from the stack
+    void pop();
+    // Check if the stack is empty 
+    bool empty(){
+        return mstack.empty(); 
+    }
+    // Return the size of the stack
+    int size(){
+        return mstack.size();
+    }
 };
 
-template <class T>
-void MinStack<T>::push(T x)
-{
+template<class T>
+void MinStack<T>::push(T x){
     /*
-       the element x is to be pushed at top of the stack
-       we push { x, MIN_ELEMENT } instead of only x.
-       here MIN_ELEMENT represents the minimum element.
+        Push the element in the main stack
+        If the aux stack is empty i.e for the first element, push on both the stacks
+        Else push on aux stack only when it is smaller or equal to the current min in aux stack
     */
+    mstack.push_back(x);
+    if(aux.empty() || aux.back() >= mstack.back())
+        aux.push_back(x);
+}
 
-    if (myStack.empty())
-        myStack.push({x, x});
-
-    else
-    {
-        T currMin = getMin();
-        myStack.push({x, min(currMin, x)});
+template<class T>
+void MinStack<T>::pop(){
+    /*
+        If the stack is not empty and top element of main stack & aux stack are same 
+        then remove the top elements from both the stack.
+        Else remove from only main stack
+    */
+    if(!mstack.empty()) {
+        if(mstack.back() == aux.back())
+            aux.pop_back();
+        mstack.pop_back();
     }
 }
 
-template <class T>
-void MinStack<T>::pop()
-{
-    if (!myStack.empty())
-        myStack.pop();
+template<class T>
+T MinStack<T>::top(){
+    // Return the topmost element of main stack.
+    if(mstack.empty())
+        throw *new Exception;
+    
+    return mstack.back();
 }
 
-template <class T>
-T MinStack<T>::top()
-{
-    //if stack is empty throw an exception
-    if (myStack.empty())
-        throw *new exception();
-
-    return myStack.top().first;
-}
-
-template <class T>
-T MinStack<T>::getMin()
-{
-    //if stack is empty throw an exception
-    if (myStack.empty())
-        throw *new exception();
-
-    return myStack.top().second;
+template<class T>
+T MinStack<T>::getMin(){
+    // Return the topmost element of aux stack.
+    if(aux.empty())
+        throw *new Exception;
+    
+    return aux.back();
 }
 
 #endif // MINSTACK_H
